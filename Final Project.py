0d2f4e74a2f1b4a9eba16df9832ffcc4fe40d450
seaboo1111 <153274594+seaboo1111@users.noreply.github.com>
2024-05-01 16:56:16 -0700

    Bug fixes

--- Final Project.py
+++ Final Project.py
@@ -12,17 +12,19 @@ def remove_url_column():
 # This function removes mostly blank rows when called
 def remove_mostly_blank_rows():
   # Replace blank spaces with NaN to remove mostly blank rows them
-  lego_df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+  lego_df.replace(r"^\s*$", np.nan, regex=True, inplace=True)
   # Drop rows with any NaN values to remove mostly blank rows
   lego_df.dropna(inplace=True)
 
 def save_new_csv(lego_inventory_df):
-    lego_df.to_csv('lego_sets_inventory.csv', index=False)
+    lego_inventory_df.to_csv("lego_sets_inventory.csv", index=False)
 
-# Load the Lego CSV file as data frame
-lego_df = pd.read_csv('lego_sets_and_themes.csv')
-
-
+# Load the Lego and lego inventory CSV file as data frame
+lego_df = pd.read_csv("lego_sets_and_themes.csv")
+lego_inventory_df = pd.read_csv("lego_sets_inventory.csv")
+#prints curent inventory
+print("Lego Inventory")
+print(lego_inventory_df)
 
 # calls fuction to remove unneeded url column
 remove_url_column()
@@ -31,28 +33,46 @@ remove_url_column()
 remove_mostly_blank_rows()
 
 # prints the list of all lego sets
+print("list of all lego sets")
 print(lego_df)
 
-# sort lego df by theme ascending
+# sort lego df and lego inventory df by theme ascending
 lego_df = lego_df.sort_values("theme_name", ascending = True)
 
-# This initializes the data frame so that that is has header data in it
-lego_inventory_df = pd.DataFrame(columns=["set_number", "set_name", "year_released", "number_of_parts", "theme_name"])
-
-# This initializes The main loop so that sets can be added to the lego inventory7
+# This initializes The main loop so that sets can be added to the lego inventory
 done = "no"
+
 while done != "yes":
-  # Asks the user which set number they want added to the inventory.
-  set_number = input("What's that number would you like to add? Don't forget to add the sub number at the end -# ")
-  # locates the set in lego_df
-  entry = lego_df.loc[lego_df['set_number'] == set_number]
-  # adds the row with the set number to inventory
-  lego_inventory_df = pd.concat([lego_inventory_df, entry])
-  # Print the updated lego inventory
-  print(lego_inventory_df)
-  # Print the updated lego inventory
-  save_new_csv(lego_inventory_df)
-  # This checks if the user wants to stop adding to the inventory
+  # checks if user wants to remove or add from inventory
+  add_or_remove = input("do you want to add or remove set from inventory? type add or remove ").lower()
+  # to add a row to the inventory
+  if add_or_remove == "add":
+    # Asks the user which set number they want added to the inventory.
+    set_number = input("What's that number would you like to add? Don't forget to add the sub number at the end -# ")
+    # locates the set in lego_df
+    entry = lego_df.loc[lego_df['set_number'] == set_number]
+    # adds the row with the set number to inventory
+    lego_inventory_df = pd.concat([lego_inventory_df, entry])
+    # Print the updated lego inventory
+    print(lego_inventory_df)
+    # save the updated lego inventory
+    save_new_csv(lego_inventory_df)
+
+  # to remove a row from the inventory
+  elif add_or_remove == "remove":
+    # These lines remove the item from inventory
+    set_number = input("What's that number would you like to remove? Don't forget to add the sub number at the end -# ")
+    lego_inventory_df = lego_inventory_df.drop(lego_inventory_df[lego_inventory_df['set_number'] == set_number].index)
+    # Print the updated lego inventory
+    print(lego_inventory_df)
+    # save the updated lego inventory
+    save_new_csv(lego_inventory_df)
+
+  # print if user does a cmd beside add and remove.
+  else:
+    print("invalid input")
+  # This checks if the user is done
   done = input("Are you done entering lego sets into the inventory? yes or no ").lower()
 
+# prints the inventory for the final time before closing program
 print(lego_inventory_df)
